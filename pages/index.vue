d67ed693fd9bff9110408e0ab3e907169914fcb8
Antonio <36239244+ASxCRO@users.noreply.github.com>
2025-06-07 23:05:39 +0200

    landing page, navbar (#50)

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventureswiththis simple travel log
+        </p>
+        <button class="btn btn-accent">
+          Sign in
+          <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>

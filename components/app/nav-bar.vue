249f87d960e2f7559778b6dca65c7af7877a5e3f
Antonio <36239244+ASxCRO@users.noreply.github.com>
2025-06-07 23:25:42 +0200

    darkMode (#51)

--- components/app/nav-bar.vue
+++ components/app/nav-bar.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import ThemeToggle from "./theme-toggle.vue";
+</script>
+
 <template>
   <div class="navbar bg-primary text-primary-content">
     <div class="navbar-start">
@@ -6,6 +10,7 @@
       </NuxtLink>
     </div>
     <div class="navbar-end">
+      <ThemeToggle />
       <a class="btn btn-accent">Sign in  <Icon name="tabler:brand-github" size="24" /></a>
     </div>
   </div>
